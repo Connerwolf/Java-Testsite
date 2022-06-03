25b679a8c98f0ced2a7a8c3572e896322d26680c
Joseph <Jsphstan679@gmail.com>
2022-06-03 19:41:19 +0530

    Big Progress

--- src/com/wolfenstien/Main.java
+++ src/com/wolfenstien/Main.java
@@ -1,14 +1,30 @@
 package com.wolfenstien;
 
-
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        boolean condition = true;
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int myNumber = (int)(Math.random()*100);
+        int userNumber = 0;
+
         do {
-            System.out.println("Spam Bot Activated");
-        }while(condition);
+            System.out.println("Guess my number(1-100) : ");
+            userNumber = sc.nextInt();
+
+            if(userNumber == myNumber) {
+                System.out.println("WOOHOO .. CORRECT NUMBER!!!");
+                break;
+            }
+            else if(userNumber > myNumber) {
+                System.out.println("your number is too large");
+            }
+            else {
+                System.out.println("your number is too small");
+            }
+        } while(userNumber >= 0);
+
+        System.out.print("My number was : ");
+        System.out.println(myNumber);
     }
 }
